4b1f5096013ef6a2e80960bb35468e8f61ba2cea
Steve Hornyak <hornyak@usc.edu>
2020-11-17 20:58:54 -0800

    Better UserEndpoint
    Updated the UserEndpoint java class

--- FinalProjBackEnd/src/UserEndpoint.java
+++ FinalProjBackEnd/src/UserEndpoint.java
@@ -1,9 +1,12 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;  
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,40 +15,23 @@ import javax.ws.rs.core.Response;
 
 @Path("/users")
 public class UserEndpoint 
-{
-	private Connection conn;
-	
-	public UserEndpoint()
-	{
-		String db = "jdbc:mysql://localhost:3036/CSCI201_Final_Database";
-		String user = "root";
-		String pwd = "root";
-		
-		conn =  DriverManager.getConnection(db, user, pwd);
-	}
-	
-	@Path("/{userID}/{$passwordHash}")
-	@GET 
+{	
+	@POST 
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public User getUser(@PathParam("userID") String userID, @PathParam("passwordHash") String passwordHash)
+	public UserResponse signInUser(SignInUser u)
 	{
-		if(Utilities.VerifyUser(Integer.parseInt(userID), conn, passwordHash))
-		{
-			return new User();
-		}
-		else {
-			return null;
-		}
+		ResultSet rs = null;
 		
-		return new User();
+		//Convert ResultSet into a User Response
+		
+		return new UserResponse();
 	}
 	
-	@POST
+	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	// Alternatively, we can set the password to null in user and return that??
-	public Message(Response) createUser(User u)
+	public UserResponse createUser(User u)
 	{
 		
 	}
